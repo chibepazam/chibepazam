bb28edf8bf1aff92a7df8b0c5c33135375bbc0bb
Parisa Kalaki <parisakalaki@gmail.com>
2023-04-02 17:06:36 +0300

    create views.

--- foods/views.py
+++ foods/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
-def index(request):
+
+def list(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+@csrf_exempt 
+def insert(request):
+    if request.method == 'POST':
+     return HttpResponse("Hello, insert.")
